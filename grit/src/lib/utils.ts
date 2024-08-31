d316731c23ebbda201a450fa4c9d0b38a2f95e20
sanghyun <sang0926@gmail.com>
2024-08-31 15:54:26 -0400

    Add landing page text+ tailwind styles

---
+++ grit/src/lib/utils.ts
@@ -0,0 +1,9 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]){
+
+    //px-2 py-2 -> p-2
+    //basically merges tailwind classes
+    return twMerge(clsx(inputs))
+}
